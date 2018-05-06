b88dbf95121de3581a0bdef6f1d5e2632909d23a
Unknown <victor.trinh.1@ens.etsmtl.ca>
2018-05-05 22:00:27 -0400

    Implementation of camera

--- screens/Camera_Screen.js
+++ screens/Camera_Screen.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux'
-import { BarCodeScanner, Permissions } from 'expo';
+import { Camera, Permissions } from 'expo';
 import { UtilStyles } from '../style/styles';
 import NavigatorService from './../utils/navigator';
 import { onBoard } from '../actions';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { RkButton } from 'react-native-ui-kitten';
 
 class Camera_Screen extends Component {
   static navigationOptions = {
@@ -19,6 +20,7 @@ class Camera_Screen extends Component {
 
   state = {
     hasCameraPermission: null,
+    type: Camera.Constants.Type.back
   }
 
   async componentWillMount() {
@@ -28,7 +30,7 @@ class Camera_Screen extends Component {
 
   render() {
     const { hasCameraPermission } = this.state;
-
+    
     if (hasCameraPermission === null) {
       return <Text>Requesting for camera permission</Text>;
     } else if (hasCameraPermission === false) {
@@ -36,10 +38,28 @@ class Camera_Screen extends Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            style={StyleSheet.absoluteFill}
-          />
+            <Camera style={{ flex: 1 }} type={this.state.type} ref={ (ref) => {this.camera = ref} }>
+                <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'column', alignItems: 'center' }}>
+                     <TouchableOpacity style={{ flex: 1, paddingTop: 20, paddingRight: 20, alignSelf: 'flex-end' }} 
+                            onPress={() => {
+                                this.setState({
+                                type: this.state.type === Camera.Constants.Type.back
+                                    ? Camera.Constants.Type.front
+                                    : Camera.Constants.Type.back,
+                            }); }}>
+                        <Icon 
+                            style={[ UtilStyles.icon, {  color: 'white'} ]}
+                            name={'undo'}
+                            size={50} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.snap.bind(this)}>
+                        <Icon 
+                            style={[ UtilStyles.icon, {  color: 'white', marginBottom: 20 } ]}
+                            name={'camera'}
+                            size={50} />
+                    </TouchableOpacity>
+                </View>     
+            </Camera>
         </View>
       );
     }
@@ -49,11 +69,17 @@ class Camera_Screen extends Component {
     this.props.navigation.navigate("main_screen");
   };
 
-  _handleBarCodeRead = ({ type, data }) => {
-    if (this.scanSuccess) return;
-    this.scanSuccess = true;
-    this.props.onBoard(data)
-  }
+  snap = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+        // photo returns Object
+        //   Object {
+        //     "height": 3468,
+        //     "uri": "file:///var/mobile/Containers/Data/Application/FEF20E3F-EFAD-4BCC-83F7-5852670E50B3/Library/Caches/ExponentExperienceData/%2540r4ge%252Frecycleit/Camera/976B14EC-EB24-4BB0-A97C-6290F400D7C2.jpg",
+        //     "width": 2304,
+        //   }
+    }
+  };
 }
 
 const styles = StyleSheet.create({
